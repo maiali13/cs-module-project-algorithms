530c11fc32b0e61b08981147fe8d190ae59c3eaa
maiali13 <maryamfawaz@gmail.com>
2020-06-18 01:11:07 -0700

    single_number alg

--- assignment/single_number/single_number.py
+++ assignment/single_number/single_number.py
@@ -2,11 +2,18 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# wednesday - first pass
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    no_dupes = []
+    # loop through the array
+    for i in arr:
+        if i not in no_dupes:
+            no_dupes.append(i)
+        # remove any number in arr that has a duplicate from no_dupes array
+        else:
+            no_dupes.remove(i)
+    # once done, the only number left in no_dupes should be our odd number out
+    return no_dupes[0]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
