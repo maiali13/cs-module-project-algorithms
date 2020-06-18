9ee8647d50b5a4b8a3bd21bf90a26886a03c663e
maiali13 <maryamfawaz@gmail.com>
2020-06-18 01:21:16 -0700

    moving_zeroes alg

--- assignment/moving_zeroes/moving_zeroes.py
+++ assignment/moving_zeroes/moving_zeroes.py
@@ -3,9 +3,10 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    # simple for loop returns the non zero integers in an array
+    # then adds as many zeroes to the output array 
+    # as there were in the original arr
+    return [i for i in arr if i != 0] + ([0]*arr.count(0))
 
 
 if __name__ == '__main__':
